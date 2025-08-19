9d06f1bef8b02e151d57feb6be64d04d5618a654
MrBowis <02alejo20@gmail.com>
2025-08-19 12:22:05 -0500

    CI/CD

--- src/utils/gradeUtils.test.js
+++ src/utils/gradeUtils.test.js
@@ -1,6 +1,6 @@
 import { calcWeightedGrade, percentile } from './gradeUtils.js';
 
-describe('calcWeightedGrade', () => {
+describe("calcWeightedGrade", () => {
     test('Caso de referencia: 80*0.4 + 90*0.6 = 86.00', () => {
         const result = calcWeightedGrade([{score: 80, weight: 0.4}, {score: 90, weight: 0.6}]);
         expect(result).toBe(86.00);
